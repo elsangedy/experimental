df057b65ae1632588fabe603c42f12120b91b01e
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-07-24 19:10:33 -0300

    store: show logout button when authenticated

--- apps/store/src/LogoutButton.tsx
+++ apps/store/src/LogoutButton.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
-import { useSetRecoilState } from 'recoil'
+import { useSetRecoilState, useRecoilValue } from 'recoil'
 
 import authAtoms from 'auth/atoms'
 
 const LogoutButton = () => {
+  const isAuthenticated = useRecoilValue(authAtoms.isAuthenticated)
   const setCurrentUser = useSetRecoilState(authAtoms.currentUser)
+
+  if (!isAuthenticated) {
+    return null
+  }
 
   return <button onClick={() => setCurrentUser('')}>logout</button>
 }
