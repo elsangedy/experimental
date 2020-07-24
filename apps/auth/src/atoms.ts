b1b2d6ebb2c0a1c847a62edc3a580d265e080d25
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-07-24 19:05:06 -0300

    auth: add isAuthenticated selector

--- apps/auth/src/atoms.ts
+++ apps/auth/src/atoms.ts
@@ -1,10 +1,20 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 const currentUser = atom({
   key: 'currentUser',
   default: 'Current User Name'
 })
 
+const isAuthenticated = selector({
+  key: 'isAuthenticated',
+  get: ({ get }) => {
+    const user = get(currentUser)
+    const authenticated = !!user && user !== ''
+    return authenticated
+  }
+})
+
 export default {
-  currentUser
+  currentUser,
+  isAuthenticated
 }
